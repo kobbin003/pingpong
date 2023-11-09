3576509df754134904255744a8aefb37e6308335
kobbin003 <dkfeto@gmail.com>
2023-11-09 19:43:07 +0530

    auth api

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,22 +1,11 @@
 import express from "express";
-import bcrypt from "bcrypt";
 import passport from "passport";
-import { randomUUID } from "crypto";
-import { User } from "../types/User";
-import { registerUser } from "../controller/user/registerUser";
-import generateJwtToken from "../utils/generateJwtToken";
-import { loginUser } from "../controller/user/loginUser";
+import { registerUser } from "../controller/auth/registerUser";
+import { loginUser } from "../controller/auth/loginUser";
 const router = express.Router();
 
 router.post("/register", registerUser);
 
-router.post("/loginjwt", loginUser);
+router.post("/login", loginUser);
 
-router.post(
-	"/checkAuth",
-	passport.authenticate("jwt", { session: false }),
-	function (req, res) {
-		res.send(req.user);
-	}
-);
 export { router as authRouter };
